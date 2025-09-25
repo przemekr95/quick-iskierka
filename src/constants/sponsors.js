ceb5d9a9685e1e08e7797f3e9075096bcee4483b
Przemek Rura <przemekrura@Mac-mini-Przemek.local>
2025-09-25 18:28:53 +0200

    Added carousel

---
+++ src/constants/sponsors.js
@@ -0,0 +1,49 @@
+import { HERO_IMAGE } from './images'
+
+// Temporary sponsors data - in future this could come from CMS or API
+export const SPONSORS_DATA = [
+  {
+    id: 1,
+    name: 'Tech Solutions',
+    logo: HERO_IMAGE,
+    website: 'https://example.com',
+  },
+  {
+    id: 2,
+    name: 'Digital Partners',
+    logo: HERO_IMAGE,
+    website: 'https://example2.com',
+  },
+  {
+    id: 3,
+    name: 'Innovation Hub',
+    logo: HERO_IMAGE,
+    website: 'https://example3.com',
+  },
+  {
+    id: 4,
+    name: 'Future Corp',
+    logo: HERO_IMAGE,
+    website: null, // Some sponsors might not have websites
+  },
+  {
+    id: 5,
+    name: 'Smart Systems',
+    logo: HERO_IMAGE,
+    website: 'https://example5.com',
+  },
+  {
+    id: 6,
+    name: 'Global Ventures',
+    logo: HERO_IMAGE,
+    website: 'https://example6.com',
+  },
+  {
+    id: 7,
+    name: 'Creative Labs',
+    logo: HERO_IMAGE,
+    website: 'https://example7.com',
+  },
+]
+
+export default SPONSORS_DATA
